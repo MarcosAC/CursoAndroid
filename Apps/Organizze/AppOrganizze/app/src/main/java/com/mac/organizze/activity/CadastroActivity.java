bc525a9b1628cebca02221fda2b5651bffee0b34
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-07-03 21:43:55 -0300

    Configurações Firebase

--- Apps/Organizze/AppOrganizze/app/src/main/java/com/mac/organizze/activity/CadastroActivity.java
+++ Apps/Organizze/AppOrganizze/app/src/main/java/com/mac/organizze/activity/CadastroActivity.java
@@ -1,12 +1,18 @@
 package com.mac.organizze.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.mac.organizze.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private EditText editNome, editEmail, editSenha;
+    private Button btnCadastrar;
+    //private Firebaseauth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
